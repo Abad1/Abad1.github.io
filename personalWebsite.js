5470556e5d3870031d13df60fcff8739e73046a2
Abad1 <abad.cody@gmail.com>
2025-02-10 09:40:26 -0900

    reformmating projects and testing link

--- personalWebsite.js
+++ personalWebsite.js
@@ -1,4 +1,20 @@
 function myFunction() {
     document.getElementById("test").innerHTML = "what da dog doin";
-    document.getElementById("roach").style.visibility = "visible";
+    document.getElementById("roach").style.display = "block";
+}
+
+const projects = document.getElementById("projects");
+
+function scrollLeft(){
+    projects.scrollBy({
+        left: -distance,
+        behavior: 'smooth'
+    });
+}
+
+function scrollRight(){
+    projects.scrollBy({
+        left: -distance,
+        behavior: 'smooth'
+    });
 }
